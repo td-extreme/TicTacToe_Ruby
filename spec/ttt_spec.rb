374f684b4d7beb57a6f1d3fcb67fdce993477cc6
Tyler Decker <tyler.decker@gmail.com>
2016-04-05 16:03:50 -0500

    Wrote test to makesure board with moves prints correctly

--- spec/ttt_spec.rb
+++ spec/ttt_spec.rb
@@ -60,6 +60,16 @@ describe TicTacToe do
       expect(subject.print_board).to eq(nil)
                                               
     end
+  
+    it "correctly outputs a board with moves played" do
+      for i in 0..8
+        subject.play_move(i, i)
+      end
+      
+      expect(STDOUT).to receive(:puts).with( " 0 | 1 | 2 \n---+---+---\n 3 | 4 | 5 \n---+---+---\n 6 | 7 | 8 \n")
+      expect(subject.print_board).to eq(nil)
+    end
+
 
   end
 
