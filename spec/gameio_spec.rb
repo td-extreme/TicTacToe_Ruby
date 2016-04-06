174db7bf9b5372d8bb30a078918b96a4cee5db84
Tyler Decker <tyler.decker@gmail.com>
2016-04-06 14:08:37 -0500

    Update gameio_spec.rb
    moved test from gameboard_spec.rb to gameio_spec.rb

--- spec/gameio_spec.rb
+++ spec/gameio_spec.rb
@@ -3,13 +3,22 @@ require 'gameio'
 
 describe GameIo do
   
-  describe "Printing a TicTacToe GameBoard" do
+  describe "Displaying the game board" do
+    
+    it "outputs a blank board when no moves have been played" do
+      expect(STDOUT).to receive(:puts).with( "   |   |   \n---+---+---\n   |   |   \n---+---+---\n   |   |   \n")
+      expect(subject.print_board).to eq(nil)
+                                              
+    end
   
-  
-
-
-  end
- 
+    it "correctly outputs a board with moves played" do
+      for i in 0..8
+        subject.play_move(i, i)
+      end
+      
+      expect(STDOUT).to receive(:puts).with( " 0 | 1 | 2 \n---+---+---\n 3 | 4 | 5 \n---+---+---\n 6 | 7 | 8 \n")
+      expect(subject.print_board).to eq(nil)
+    end
  
 
 end
