9e9cd0b8cdb21c6bba8830f63f171d9919676105
Tyler Decker <tyler.decker@gmail.com>
2016-04-06 14:11:50 -0500

    Update gameio.rb
    Added print_board function

--- lib/gameio.rb
+++ lib/gameio.rb
@@ -1,8 +1,22 @@
 # ./lib/io.rb
+require 'gameboard'
 
 class GameIo 
 
+  # todo :: move this to an IO Class
+  def print_board (gameboard)
+    rtn_string = ""
+    i = 0
+    begin
 
+       rtn_string << " #{@board[i]} | #{@board[i+1]} | #{@board[i+2]} \n"
+       rtn_string << "---+---+---\n" if i < 6
+       i = i + 3
+    end while i <=6
+    
+    puts rtn_string
+
+  end
 
 
 end
