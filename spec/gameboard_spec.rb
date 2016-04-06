88387f05c15e52869887513f45cc6de5a76a04d1
Tyler Decker <tyler.decker@gmail.com>
2016-04-06 14:09:36 -0500

    Update gameboard_spec.rb
    Removed test for printing a game board.

--- spec/gameboard_spec.rb
+++ spec/gameboard_spec.rb
@@ -112,31 +112,5 @@ describe GameBoard do
       end
       expect(subject.game_state).to eq('Tied')
     end
-
-
-
- 
   end
-
-  describe "Displaying the game board" do
-    
-    it "outputs a blank board when no moves have been played" do
-      expect(STDOUT).to receive(:puts).with( "   |   |   \n---+---+---\n   |   |   \n---+---+---\n   |   |   \n")
-      expect(subject.print_board).to eq(nil)
-                                              
-    end
-  
-    it "correctly outputs a board with moves played" do
-      for i in 0..8
-        subject.play_move(i, i)
-      end
-      
-      expect(STDOUT).to receive(:puts).with( " 0 | 1 | 2 \n---+---+---\n 3 | 4 | 5 \n---+---+---\n 6 | 7 | 8 \n")
-      expect(subject.print_board).to eq(nil)
-    end
-
-
-  end
-
-
 end
